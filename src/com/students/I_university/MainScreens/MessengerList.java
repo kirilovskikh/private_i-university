77495e880071439d0361a0bc09139db33e06f250
sveta <swsweta@live.com>
2013-08-15 23:36:44 +0600

    cписок контактов

--- src/com/students/I_university/MainScreens/MessengerList.java
+++ src/com/students/I_university/MainScreens/MessengerList.java
@@ -25,6 +25,8 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.students.I_university.Utils.convertStreamToString;
+
 /**
  * Created with IntelliJ IDEA.
  * User: kirilovskikh
@@ -38,90 +40,72 @@ public class MessengerList extends SherlockFragment {
     String web_way = "http://university.shiva.vps-private.net/webservice/rest/server.php?";
 
     String str;
+    String token = "ac072b83ec6761808d3994dc446557f9";
 
-
-
-    @Override
-    protected Void doInBackground(Void... voids) {
-        try {
-
-            HttpClient httpClient = new DefaultHttpClient();
-            HttpPost httpPost = new HttpPost(url);
-
-            List<NameValuePair> list = new ArrayList<NameValuePair>();
-
-            list.add(new BasicNameValuePair("wstoken", MyActivity.prefs.getString("token", null)));
-
-//            list.add(new BasicNameValuePair("wsfunction", "moodle_enrol_get_users_courses"));
-//            list.add(new BasicNameValuePair("userid", "2"));
-
-//            list.add(new BasicNameValuePair("wsfunction", "moodle_webservice_get_siteinfo"));
-
-            //list.add(new BasicNameValuePair("wsfunction", "moodle_user_get_users_by_id"));
-            list.add(new BasicNameValuePair("wsfunction", "local_wstemplate_recent_messages"));
-            list.add(new BasicNameValuePair("limitfrom", "0"));
-            list.add(new BasicNameValuePair("limitto", "0")) ;
-
-//            list.add(new BasicNameValuePair("userids[0]", "9"));
-//            list.add(new BasicNameValuePair("userids[1]", "4"));
-
-            list.add(new BasicNameValuePair("moodlewsrestformat", "json"));
-
-            httpPost.setEntity(new UrlEncodedFormEntity(list, "UTF-8"));
-            HttpResponse httpResponse = httpClient.execute(httpPost);
-
-            if (httpResponse != null) {
-                InputStream in = httpResponse.getEntity().getContent(); // Get the
-                str = convertStreamToString(in);
-                JSONArray jsonArray = new JSONArray(str);
-
-
-
-
-                JSONObject jsonObject = jsonArray.getJSONObject(0);
-                for (int i=0; i< jsonArray.length(); i++)
-                {
-                    String s = jsonObject.getString("firstname");
-                    String s1 = jsonObject.getString("lastname");
-                    String s2 = jsonObject.getString("smallmessage");
-                    String s3 = jsonObject.getString("timecreated");
-                    str = s3;
-                }
-            }
-
-        }
-        catch (Exception e) {
-            str = "exp";
-        }
-
-
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.listview_layout, null);
-
-        final String[] kontakt = new String[] {"Старикова Анастасия Константиновна", "Сидоров Петр Сергеевич", "Петров Алексей Федорович", "Сидорова Дарья Ивановна"};
-
-        final String[] sms = new String[] {"Hello","Hi","Привет","Ура"};
-
-        ListView kontaktList = (ListView) view.findViewById(R.id.listView);
-        CustomAdapterDialogs adapter = new CustomAdapterDialogs(getSherlockActivity(), R.layout.sms,R.id.textView,R.id.textView1,kontakt,sms);
+       try {
 
 
-        // устанавливаем для списка адаптер
-        kontaktList.setAdapter(adapter);
+           HttpClient httpClient = new DefaultHttpClient();
+           HttpPost httpPost = new HttpPost(web_way);
 
-        kontaktList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(getSherlockActivity(), dialogActivity.class);
-                //  intent.putExtra("name", strings[position]);
-                startActivity(intent);
-            }
-        });
-        return view;
+           List<NameValuePair> list = new ArrayList<NameValuePair>();
+           list.add(new BasicNameValuePair("wstoken", token));
+           list.add(new BasicNameValuePair("moodlewsrestformat", "json"));
+
+           list.add(new BasicNameValuePair("wsfunction", "local_wstemplate_recent_messages"));
+           list.add(new BasicNameValuePair("limitfrom", "0"));
+           list.add(new BasicNameValuePair("limitto", "0"));
+
+           httpPost.setEntity(new UrlEncodedFormEntity(list, "UTF-8"));
+           HttpResponse httpResponse = httpClient.execute(httpPost);
+
+           String[] kontakt = new String[0];
+           String[] sms = new String[0];
+           if (httpResponse != null) {
+               InputStream in = httpResponse.getEntity().getContent(); // Get the
+               str = convertStreamToString(in);
+               JSONArray jsonArray = new JSONArray(str);
+
+               sms = new String[jsonArray.length()];
+               kontakt = new String[jsonArray.length()];
+
+               for (int i = 0; i < jsonArray.length(); i++) {
+                   JSONObject jsonObject = jsonArray.getJSONObject(i);
+                   kontakt[i] = jsonObject.getString("firstname") + jsonObject.getString("lastname");
+                   sms[i] = jsonObject.getString("smallmessage");
+                   //         String s3 = jsonObject.getString("timecreated");
+               }
+           }
+           //  return null;  //To change body of implemented methods use File | Settings | File Templates.
+           //  final String[] kontakt = new String[] {"Старикова Анастасия Константиновна", "Сидоров Петр Сергеевич", "Петров Алексей Федорович", "Сидорова Дарья Ивановна"};
+           // final String[] sms = new String[] {"Hello","Hi","Привет","Ура"};
+
+           View view = inflater.inflate(R.layout.listview_layout, null);
+           ListView kontaktList = (ListView) view.findViewById(R.id.listView);
+           CustomAdapterDialogs adapter = new CustomAdapterDialogs(getSherlockActivity(), R.layout.sms, R.id.textView, R.id.textView1, kontakt, sms);
+
+
+           // устанавливаем для списка адаптер
+           kontaktList.setAdapter(adapter);
+
+           kontaktList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+               @Override
+               public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                   Intent intent = new Intent(getSherlockActivity(), dialogActivity.class);
+                   //  intent.putExtra("name", strings[position]);
+                   startActivity(intent);
+               }
+           });
+
+           return view;
+       }
+       catch (Exception e) {
+           str = "exp";
+       }
+      return null;
     }
 }
