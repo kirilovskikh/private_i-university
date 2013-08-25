d2584694a220090140383f9fa085f0ef039487ac
Max Ikonnikov <lastick1@gmail.com>
2013-08-25 22:31:41 +0600

    bugfixes

--- src/com/students/I_university/AuthorizationActivity.java
+++ src/com/students/I_university/AuthorizationActivity.java
@@ -110,4 +110,12 @@ public class AuthorizationActivity extends SherlockActivity {
         });
 
     }
+
+    public void onBackPressed(){
+        super.onBackPressed();
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
 }
